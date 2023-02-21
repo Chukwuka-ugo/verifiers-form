84d8f643b71beea4626a729104e1cc2d8d18ef46
Chukwuka <chuxjpu@gmail.com>
2023-02-21 14:26:11 +0100

    This is a form uses google spreadsheet as dataBase

---
+++ assets/js/axio.js
@@ -0,0 +1,123 @@
+
+// this was a trial done and an actual long process which probably isnt 80% certain
+
+//             const axios = require('axios');
+
+
+    //            function required()
+    //            {
+    //               const form = document.forms["google-sheet"]['centerCode'].value;
+    //                const form1 = document.forms["google-sheet"];
+    //                
+    //                console.log(form1);
+    //                
+    //                if(form !== "")
+    //                   {
+    //                       console.log(form);
+    //                       if(form === "335")
+    //                         {
+    //
+    //                             console.log("yes o "+form+" for delta");
+    //                             
+    ////                            fetch(asabaURL, { method: 'POST', body: new FormData(form1)})
+    ////                            .then(response => alert("Thanks for submitting your form from Asaba..! Your verification will be validated soon..."))
+    ////                            .catch(error => console.error('Error!', error.message))
+    //                             
+    ////                            axios.post(asabaURL, {
+    ////    Name: Name
+    ////  })
+    ////  .then(function (response) {
+    ////    console.log(response);
+    ////  })
+    ////  .catch(function (error) {
+    ////    console.log(error);
+    ////  });
+    //
+    //                            alert("yes o "+form+" for delta");
+    //                             
+    //                         }else if(form === "524")
+    //                         {
+    //
+    //                            /*fetch(lagosURL, { method: 'POST', body: new FormData(form)})
+    //                            .then(response => alert("Thanks for submitting your form from Lagos..! Your verification will be validated soon..."))
+    //                            .catch(error => console.error('Error!', error.message))*/
+    //
+    //                            alert("yes o "+form+" for lagos");
+    //                         }
+    //                       else
+    //                           {
+    //                               alert("this "+form+"is not the right code");
+    //                           }
+    //                           alert("okay oo "+form);
+    ////                               return true;
+    //
+    //                   }else
+    //                    {
+    //                
+    //                        alert("add ur code");
+    ////                        return false;
+    //                        
+    //                    }
+
+
+    //            }
+
+
+                   /* const form = document.forms['google-sheet'];
+
+                     const asabaURL = 'https://script.google.com/macros/s/AKfycbw61uhixOA6sIsHprbEf5HHYHh59VYElp45C9_RIW3yQ7RSI7sq1Az-WQ/exec';
+
+                const lagosURL = 'https://script.google.com/macros/s/AKfycbzXvATt2z-d-ibs_KYAIXbtwTaSjUq_Y5PnDCn42N4vXO5GrUr-2Uz6KA/exec';*/
+
+
+                    /*if(form != "")
+                       {
+                           console.log("here is ur code "+ form);
+                           if(form === "335")
+                             {
+                                console.log("yes o "+form+" for delta");
+                                fetch(asabaURL, { method: 'POST', body: new FormData(form)})
+                                .then(response => alert("Thanks for submitting your form from Asaba..! Your verification will be validated soon..."))
+                                .catch(error => console.error('Error!', error.message));
+
+                              alert("yes o "+form+" for delta");
+                             }
+                           else if(form == 524)
+                               {
+
+                                        fetch(lagosURL, { method: 'POST', body: new FormData(form)})
+                                    .then(response => alert("Thanks for submitting your form from Lagos..! Your verification will be validated soon..."))
+                                    .catch(error => console.error('Error!', error.message))
+
+                                    alert("yes o "+form+" for lagos");
+                               }
+                           else
+                               {
+                                   alert("this "+form+" is not the right code");
+                               }
+                       alert("okay oo "+form);
+
+
+                       }
+                    else //if(form == "") 
+                        {
+
+                            alert("add ur code");
+                            return false;
+
+                        }
+
+
+                /*form.addEventListener('submit', e => {
+                  e.preventDefault();
+
+                    if(cod === "pa"){
+                       console ("welcome")
+                       }
+                  fetch(asabaURL, { method: 'POST', body: new FormData(form)})
+                    .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+                    .catch(error => console.error('Error!', error.message))
+                })*/
+
+
+
